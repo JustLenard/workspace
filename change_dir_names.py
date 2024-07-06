837b2b8c095ffed8dc6c0a2afc433b5dfb52afa2
JustLenard <justlenard.justme@gmail.com>
2024-07-07 02:05:20 +0300

    Added path. Added wallpaper

--- change_dir_names.py
+++ change_dir_names.py
@@ -1,6 +1,9 @@
 import os
 
-res = os.listdir("../")
+
+path = "../003_archive/"
+res = os.listdir(path)
+
 
 # os.mkdir("../testDir")
 
@@ -12,4 +15,4 @@ res = os.listdir("../")
 for i in range(1, len(res) + 1):
     new_name = f"{i:03}_{res[i -1]}"
     print("\033[93m\033[1m >> \033[0m", new_name)
-    os.rename(f"../{res[i -1]}", f"../{new_name}")
+    os.rename(f"{path}{res[i -1]}", f"{path}{new_name}")
